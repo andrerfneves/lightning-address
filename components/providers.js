55e1ac30edc7f5a827565359cdd8a867bfb9e1b3
LightningTipBot <88730856+LightningTipBot@users.noreply.github.com>
2021-09-20 19:56:54 +0200

    Update providers.js
    providers

--- components/providers.js
+++ components/providers.js
@@ -265,6 +265,13 @@ export const Providers = () => (
           <ProviderSignUpButton isDisabled>Coming Soon</ProviderSignUpButton>
         </ProviderCard>
       </ProvidersLeft>
+        <ProviderCard>
+          <ImageWrapper>
+            <ZEBEDEEImage src={'/images/lightningtipbot.svg'} alt="@LightningTipBot" style={{ marginBottom: '-5px' }} />
+            <DomainURL>you@ln.tips</DomainURL>
+          </ImageWrapper>
+          <ProviderSignUpButton target="_blank" href="https://t.me/LightningTipBot">Open Telegram</ProviderSignUpButton>
+        </ProviderCard>      
       <ProvidersRight>
         <ProvidersRightInner>
           <ProvidersTitle>Your app doesn't support Lightning Addresses yet?</ProvidersTitle>
