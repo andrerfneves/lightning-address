13413bc374e0666f6dcdfa2ab1be7705b1c9648e
Lennart Lopin <novalis78@gmail.com>
2024-03-27 10:11:31 -0400

    bytewallet and bf.cash references added

--- components/hero.js
+++ components/hero.js
@@ -263,6 +263,7 @@ export class Hero extends PureComponent {
               <LoopedTextPart>starbackr.me</LoopedTextPart>
               <LoopedTextPart>lifpay.me</LoopedTextPart>
               <LoopedTextPart>vipsats.app</LoopedTextPart>
+              <LoopedTextPart>bf.cash</LoopedTextPart>
             </TextLoop>
           </LoopWrapper>
         </Fade>
