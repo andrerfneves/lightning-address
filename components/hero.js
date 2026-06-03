59e0f25ba4290c86e116e97fc78aa01198cc754a
André Neves <andrerfneves@protonmail.com>
2026-06-03 06:44:18 -0700

    refactor: extract shared CTAPrimary styled component (#176)

--- components/hero.js
+++ components/hero.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import styled from 'styled-components';
 
-import { media } from '../utils';
+import { media, CTAPrimary } from '../utils';
 
 const Wrapper = styled.div`
   display: flex;
@@ -120,30 +120,6 @@ const CTAWrapper = styled.div`
 
   ${media.tablet`
     flex-direction: row;
-  `}
-`;
-
-const CTAPrimary = styled.a`
-  color: #fff;
-  height: 2.81rem;
-  cursor: pointer;
-  padding: 0;
-  width: 260px;
-  text-align: center;
-  margin: 0 0 10px 0;
-  border-radius: 7px;
-  line-height: 2.8rem;
-  text-decoration: none;
-  background-color: #0070f3;
-  box-shadow: 0 4px 14px 0 rgb(0 118 255 / 39%);
-
-  &:hover {
-    background: rgba(0,118,255,0.9);
-    box-shadow: 0 6px 20px rgb(0 118 255 / 23%);
-  }
-
-  ${media.tablet`
-    margin: 0 15px 0 0;
   `}
 `;
 
