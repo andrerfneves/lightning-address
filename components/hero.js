a16a81870d7b9927b9fb20b63b27ee885d39a991
André Neves <andrerfneves@protonmail.com>
2026-06-04 10:48:35 -0700

    refactor: extract shared CTASecondary styled component (#181)

--- components/hero.js
+++ components/hero.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import styled from 'styled-components';
 
-import { media, CTAPrimary } from '../utils';
+import { media, CTAPrimary, CTASecondary } from '../utils';
 
 const Wrapper = styled.div`
   display: flex;
@@ -120,30 +120,6 @@ const CTAWrapper = styled.div`
 
   ${media.tablet`
     flex-direction: row;
-  `}
-`;
-
-const CTASecondary = styled.a`
-  color: #696969;
-  cursor: pointer;
-  height: 2.81rem;
-  background: #fff;
-  width: 260px;
-  text-align: center;
-  padding: 0;
-  margin: 10px 0 0 0;
-  text-decoration: none;
-  line-height: 2.8rem;
-  border-radius: 7px;
-  box-shadow: 0 4px 14px 0 rgb(0 0 0 / 10%);
-
-  &:hover {
-    background: rgba(255,255,255,0.9);
-    box-shadow: 0 6px 20px rgb(93 93 93 / 23%);
-  }
-
-  ${media.tablet`
-    margin: 0 0 0 15px;
   `}
 `;
 
